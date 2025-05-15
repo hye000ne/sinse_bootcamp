a8dc7a353e51a4e14f2576110d5cf2b9abea5588
hye000ne <hye000ne@hanmail.net>
2025-05-15 11:46:24 +0900

    [JAVA] 상속 실습

---
+++ back_workspace/java_workspace/day0515/src/animal/Bird.java
@@ -0,0 +1,14 @@
+package animal;
+
+public class Bird {
+	private String name = "부모 새";
+	public int age = 5;
+
+	public void fly(){
+		System.out.println("부모 새가 날아요.");
+	}
+
+	public void eat(){
+		System.out.println("밥 머거요");
+	}
+}
