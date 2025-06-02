1398c5b3da4306fadfb8226e0fedd25ac4404cdf
hye000ne <hye000ne@hanmail.net>
2025-06-02 17:36:00 +0900

    [JAVA] DBTest / Collection

--- back_workspace/java_workspace/testapp/src/animal/Dog.java
+++
@@ -1,7 +0,0 @@
-package animal;
-//패키지화 시킨 클래스는 외부의 다른 클래스들이 사용해야하므로 public으로 접근을 풀어준다.
-public class Dog {
-	public void bark(){
-		System.out.println("bow wow");
-	}
-}
--- back_workspace/java_workspace/testapp/src/use/UseDog.java
+++
@@ -1,8 +0,0 @@
-package use; //개발자가 패키지를 선언하면 javac -d 옵션사용 시 선언한 패키지에 해당하는 디렉토리를 자동 생성해준다.
-import animal.Dog; //환경변수 classpath 기준으로 밑의 animal 밑의 Dog.class를 임포트한다.
-class UseDog {
-	public static void main(String[] args){
-		Dog d = new Dog();
-		d.bark();
-	}
-}
